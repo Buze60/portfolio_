1f6d4d7a82a06d90bd9b32a78b619e256b750779
fasikakassahun <fasikakassahun1912@gmail.com>
2026-07-12 11:05:09 +0300

    fix: add allowedDevOrigins for dev

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: process.cwd(),
+  allowedDevOrigins: ["172.18.112.1"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
